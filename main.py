dd0d88e9d5a9e7ecf01fa853e8f675f8475c8eb2
Benjamin Kampmann <ben.kampmann@gmail.com>
2014-11-08 21:03:25 -0600

    defer based config loader

--- main.py
+++ main.py
@@ -6,13 +6,17 @@ sys.path.insert(0, os.path.join(os.path.abspath(
                 os.path.dirname('.')), "geoffrey"))
 
 
+# then import geoffrey
 from geoffrey.config import Config
 from geoffrey.api.server import GeoffreyApi, app
+
+from twisted.internet import defer
 
 
 # now patch the config_getter
 def patched_config_getter(self, request):
-    return Config({"API_KEY": "yay"})
+    request.config = Config({"API_KEY": "yay"})
+    return defer.succeed(request)
 
 
 GeoffreyApi._get_config = patched_config_getter

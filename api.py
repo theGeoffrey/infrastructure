fd9788cff5cfd3c3dede006704c4939e5b31d9ae
anoukruhaak <anoukruhaak@gmail.com>
2014-11-13 00:13:59 -0600

    update config to work for mailchimp

--- api.py
+++ api.py
@@ -8,12 +8,19 @@ from twisted.internet import defer
 
 # now patch the config_getter
 def patched_config_getter(self, request):
-    request.config = Config({"API_KEY": "yay"})
+    request.config = Config({"api_key": "yay",
+                             "enabled_services": ["mailchimp_subscribe"],
+                             "apps":
+                             {"mailchimp":
+                              {"API_KEY":
+                               "4f34ea944dd1e33a5452550789042f9c-us9",
+                               "DATA_CENTER": "us9",
+                               "TEST_LIST_ID": "f4b255a33a"}}})
+
     return defer.succeed(request)
 
 
 GeoffreyApi._get_config = patched_config_getter
-
 
 # then start serving
 API_SERVER = app.resource
